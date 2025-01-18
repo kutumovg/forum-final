7fa118128420a7a48a60dcdcc448325f8d4764ae
kutumovg <53043779+kutumovg@users.noreply.github.com>
2025-01-18 19:40:04 +0500

    Update main.go

--- main.go
+++ main.go
@@ -37,59 +37,49 @@ func main() {
 
 	// Routes
 	wrappedMux := http.NewServeMux()
-	wrappedMux.HandleFunc("/", handlers.MainPageHandler)
-	wrappedMux.HandleFunc("/register", handlers.RegisterHandler)
-	wrappedMux.HandleFunc("/login", handlers.LoginHandler)
-	wrappedMux.HandleFunc("/logout", handlers.LogoutHandler)
-	wrappedMux.HandleFunc("/create_post", handlers.CreatePostHandler)
-	wrappedMux.HandleFunc("/new_post", handlers.CreatePostPageHandler)
-	wrappedMux.HandleFunc("/post", handlers.PostPageHandler)
-	wrappedMux.HandleFunc("/like", handlers.LikeHandler)
-	wrappedMux.HandleFunc("/dislike", handlers.DislikeHandler)
-	wrappedMux.HandleFunc("/create_comment", handlers.CreateCommentHandler)
-	wrappedMux.HandleFunc("/like_comment", handlers.LikeCommentHandler)
-	wrappedMux.HandleFunc("/dislike_comment", handlers.DislikeCommentHandler)
-	wrappedMux.HandleFunc("/my_posts", handlers.MyPostsHandler)
-	wrappedMux.HandleFunc("/liked_posts", handlers.LikedPostsHandler)
-	wrappedMux.HandleFunc("/users", handlers.UsersPageHandler)
-	wrappedMux.HandleFunc("/promote", handlers.PromoteToModeratorHandler)
-	wrappedMux.HandleFunc("/demote", handlers.DemoteToUserHandler)
-	wrappedMux.HandleFunc("/admin/categories", handlers.AdminCategoriesHandler)
-	wrappedMux.HandleFunc("/admin/categories/add", handlers.AddCategoryHandler)
-	wrappedMux.HandleFunc("/admin/categories/delete", handlers.DeleteCategoryHandler)
-	wrappedMux.HandleFunc("/admin/categories/update", handlers.UpdateCategoryHandler)
-	wrappedMux.HandleFunc("/admin/categories/controversial", handlers.SetControversialHandler)
-	wrappedMux.HandleFunc("/admin/pending", handlers.GetPendingPostsHandler)
-	wrappedMux.HandleFunc("/admin/approve", handlers.ApprovePostHandler)
-	wrappedMux.HandleFunc("/admin/delete_post", handlers.DeletePostHandler)
-	wrappedMux.HandleFunc("/admin/delete_comment", handlers.DeleteCommentHandler)
-	wrappedMux.HandleFunc("/admin/unapprove", handlers.MarkPostForModerationHandler)
-	wrappedMux.HandleFunc("/moderator", handlers.RenderApplyModeratorPageHandler)
-	wrappedMux.HandleFunc("/moderator/applications", handlers.RenderModeratorApplicationsPageHandler)
-	wrappedMux.HandleFunc("/moderator/approve", handlers.ApproveModeratorApplicationHandler)
-	wrappedMux.HandleFunc("/moderator/reject", handlers.RejectModeratorApplicationHandler)
-	wrappedMux.HandleFunc("/apply_moderator", handlers.ApplyModeratorHandler)
-	wrappedMux.HandleFunc("/auth/google/login", handlers.GoogleLoginHandler)
-	wrappedMux.HandleFunc("/auth/google/callback", handlers.GoogleCallbackHandler)
-	wrappedMux.HandleFunc("/auth/github/login", handlers.GitHubLoginHandler)
-	wrappedMux.HandleFunc("/auth/github/callback", handlers.GitHubCallbackHandler)
+
+	wrappedMux.Handle("/ui/", http.StripPrefix("/ui/", http.FileServer(http.Dir("./ui"))))
+	wrappedMux.Handle("/uploads/", http.StripPrefix("/uploads/", http.FileServer(http.Dir("uploads"))))
+
+	rateLimitedMux := http.NewServeMux()
+	rateLimitedMux.HandleFunc("/", handlers.MainPageHandler)
+	rateLimitedMux.HandleFunc("/register", handlers.RegisterHandler)
+	rateLimitedMux.HandleFunc("/login", handlers.LoginHandler)
+	rateLimitedMux.HandleFunc("/logout", handlers.LogoutHandler)
+	rateLimitedMux.HandleFunc("/create_post", handlers.CreatePostHandler)
+	rateLimitedMux.HandleFunc("/new_post", handlers.CreatePostPageHandler)
+	rateLimitedMux.HandleFunc("/post", handlers.PostPageHandler)
+	rateLimitedMux.HandleFunc("/like", handlers.LikeHandler)
+	rateLimitedMux.HandleFunc("/dislike", handlers.DislikeHandler)
+	rateLimitedMux.HandleFunc("/create_comment", handlers.CreateCommentHandler)
+	rateLimitedMux.HandleFunc("/like_comment", handlers.LikeCommentHandler)
+	rateLimitedMux.HandleFunc("/dislike_comment", handlers.DislikeCommentHandler)
+	rateLimitedMux.HandleFunc("/my_posts", handlers.MyPostsHandler)
+	rateLimitedMux.HandleFunc("/liked_posts", handlers.LikedPostsHandler)
+	rateLimitedMux.HandleFunc("/users", handlers.UsersPageHandler)
+	rateLimitedMux.HandleFunc("/promote", handlers.PromoteToModeratorHandler)
+	rateLimitedMux.HandleFunc("/demote", handlers.DemoteToUserHandler)
+	rateLimitedMux.HandleFunc("/admin/categories", handlers.AdminCategoriesHandler)
+	rateLimitedMux.HandleFunc("/admin/categories/add", handlers.AddCategoryHandler)
+	rateLimitedMux.HandleFunc("/admin/categories/delete", handlers.DeleteCategoryHandler)
+	rateLimitedMux.HandleFunc("/admin/categories/update", handlers.UpdateCategoryHandler)
+	rateLimitedMux.HandleFunc("/admin/categories/controversial", handlers.SetControversialHandler)
+	rateLimitedMux.HandleFunc("/admin/pending", handlers.GetPendingPostsHandler)
+	rateLimitedMux.HandleFunc("/admin/approve", handlers.ApprovePostHandler)
+	rateLimitedMux.HandleFunc("/admin/delete_post", handlers.DeletePostHandler)
+	rateLimitedMux.HandleFunc("/admin/delete_comment", handlers.DeleteCommentHandler)
+	rateLimitedMux.HandleFunc("/admin/unapprove", handlers.MarkPostForModerationHandler)
+	rateLimitedMux.HandleFunc("/moderator", handlers.RenderApplyModeratorPageHandler)
+	rateLimitedMux.HandleFunc("/moderator/applications", handlers.RenderModeratorApplicationsPageHandler)
+	rateLimitedMux.HandleFunc("/moderator/approve", handlers.ApproveModeratorApplicationHandler)
+	rateLimitedMux.HandleFunc("/moderator/reject", handlers.RejectModeratorApplicationHandler)
+	rateLimitedMux.HandleFunc("/apply_moderator", handlers.ApplyModeratorHandler)
+	rateLimitedMux.HandleFunc("/auth/google/login", handlers.GoogleLoginHandler)
+	rateLimitedMux.HandleFunc("/auth/google/callback", handlers.GoogleCallbackHandler)
+	rateLimitedMux.HandleFunc("/auth/github/login", handlers.GitHubLoginHandler)
+	rateLimitedMux.HandleFunc("/auth/github/callback", handlers.GitHubCallbackHandler)
 	
-	http.Handle("/ui/", http.StripPrefix("/ui/", http.FileServer(http.Dir("./ui"))))
-	http.Handle("/uploads/", http.StripPrefix("/uploads/", http.FileServer(http.Dir("uploads"))))
-
-	secureMux := RateLimiter(wrappedMux)
-
-	// // HTTPS server
-	// log.Println("Starting server on https://localhost:8080")
-	// err = http.ListenAndServeTLS(
-	// 	":8080",
-	// 	"/etc/letsencrypt/live/forum.qarjy.kz/fullchain.pem", // Path to SSL certificate
-	// 	"/etc/letsencrypt/live/forum.qarjy.kz/privkey.pem",   // Path to SSL private key
-	// 	secureMux,
-	// )
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	wrappedMux.Handle("/", RateLimiter(rateLimitedMux))
 
 	go func() {
 		log.Println("Starting HTTPS server on https://localhost:443...")
@@ -97,7 +87,7 @@ func main() {
 			":443",
 			"/certs/fullchain1.pem",
 			"/certs/privkey1.pem",
-			secureMux,
+			wrappedMux,
 		)
 		if err != nil {
 			log.Fatalf("Failed to start HTTPS server: %v", err)
